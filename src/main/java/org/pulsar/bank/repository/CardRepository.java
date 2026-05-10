7ee7aef18916f953a0ff72c941417c442be34024
pulsar <pulsarmn@yandex.ru>
2026-05-10 11:57:23 +0300

    feat: add a CardRepository interface

---
+++ src/main/java/org/pulsar/bank/repository/CardRepository.java
@@ -0,0 +1,10 @@
+package org.pulsar.bank.repository;
+
+import org.pulsar.bank.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CardRepository extends JpaRepository<Card, UUID> {
+
+}
