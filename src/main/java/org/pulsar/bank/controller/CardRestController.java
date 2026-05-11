e9bdadac405f9f6f42a2957f1da722268138cd30
pulsar <pulsarmn@yandex.ru>
2026-05-11 08:37:09 +0300

    refactor: rename the CardsRestController class to CardRestController

--- src/main/java/org/pulsar/bank/controller/CardRestController.java
+++ src/main/java/org/pulsar/bank/controller/CardRestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/cards")
 @RequiredArgsConstructor
-public class CardsRestController {
+public class CardRestController {
 
     private final CardService cardService;
 
